a08779173bfcd2a28fc60ab2e1a839a6e3a64ee8
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-03-04 00:16:00 +0100

    Book class fields + getters/setters

---
+++ BookShare/console-app/src/main/java/MotoHafty/Book.java
@@ -0,0 +1,74 @@
+package MotoHafty;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private String name;
+    private List<String> authors;
+    private String category;
+    private String isbn;
+    private String id;
+    private LocalDateTime inputDate;
+
+    public Book(String title, String name, List<String> authors, String category, String isbn, String id, LocalDateTime inputDate) {
+        this.title = title;
+        this.name = name;
+        this.authors = authors;
+        this.category = category;
+        this.isbn = isbn;
+        this.id = id;
+        this.inputDate = inputDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getInputDate() {
+        return inputDate;
+    }
+
+}
