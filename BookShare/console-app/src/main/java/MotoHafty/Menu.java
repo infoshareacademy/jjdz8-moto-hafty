030df0359e87909968b7e602128c60dc8972e885
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-03-04 02:04:29 +0100

    input mismatch resistance + looped asking for number

--- BookShare/console-app/src/main/java/MotoHafty/Menu.java
+++ BookShare/console-app/src/main/java/MotoHafty/Menu.java
@@ -1,5 +1,6 @@
 package MotoHafty;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -8,14 +9,14 @@ public class Menu {
     }
 
     public void start() {
+        System.out.println("########################################################");
+        System.out.println("Cześć, jestem BookShare. ");
+        System.out.println("Daj znać co chcesz zrobić, wybierając odpowiednią cyfrę:");
         printMainMenuOptions();
         askUserForNumber();
     }
 
-    public void printMainMenuOptions(){
-        System.out.println("########################################################");
-        System.out.println("Cześć, jestem BookShare. ");
-        System.out.println("Daj znać co chcesz zrobić, wybierając odpowiednią cyfrę:");
+    public void printMainMenuOptions() {
         System.out.println("1 - Dodaj nową książkę");
         System.out.println("2 - Wyszukaj książkę w biblioteczce");
         System.out.println("3 - Zaktualizuj dane o książce");
@@ -26,9 +27,30 @@ public class Menu {
     }
 
     public Integer askUserForNumber() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("wybierz odpowiednią liczbę:");
-        return scanner.nextInt();
-    }
+        boolean isNumberPicked = false;
+        Integer number = null;
+        while (!isNumberPicked) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Wybierz odpowiednią cyfrę:");
+                number = scanner.nextInt();
+                if (number >= 1 && number <= 6) {
+                    System.out.println("Wybrano opcję nr " + number);
+                    isNumberPicked = true;
+                } else {
+                    System.out.println("Wybrano nieodpowiednią cyfrę, spróbuj jeszcze raz:");
+                    printMainMenuOptions();
+                    isNumberPicked = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wprowadzono błędną wartość");
+                System.out.println("Spróbuj jeszcze raz:");
+                printMainMenuOptions();
+                isNumberPicked = false;
 
+            }
+        }
+        return number;
+
+    }
 }
