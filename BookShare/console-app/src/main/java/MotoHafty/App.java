00cc438b5f9c98f6583354a5764a030e397feb5d
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-03-05 23:15:55 +0100

    user can print details of all books

--- BookShare/console-app/src/main/java/MotoHafty/App.java
+++ BookShare/console-app/src/main/java/MotoHafty/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         new AddNewBook().addNewBook();
         Library library = new Library();
-        System.out.println(library.getBooks().values().toString());
+        library.printAllBooks();
     }
 
 
--- BookShare/console-app/src/main/java/MotoHafty/Library.java
+++ BookShare/console-app/src/main/java/MotoHafty/Library.java
@@ -18,4 +18,21 @@ public class Library {
     public Map<Integer, Book> getBooks() {
         return books;
     }
+
+    public void printAllBooks (){
+        System.out.println("_________________________________");
+        System.out.println("W Biblioteczce znajdują się następujące ksiązki:");
+        for (Map.Entry<Integer,Book> entry : books.entrySet()) {
+            System.out.println("#########################################");
+            System.out.println("ID: " + entry.getKey());
+            System.out.println("Tytuł: " + entry.getValue().getTitle());
+            System.out.println("Główny autor: " + entry.getValue().getAuthors().get(0));
+            System.out.println("Wszyscy autorzy: " + entry.getValue().getAuthors().toString());
+            System.out.println("Kategoria: " + entry.getValue().getCategory());
+            System.out.println("ISBN: " + entry.getValue().getIsbn());
+            System.out.println("Data dodania: " + entry.getValue().getInputDate());
+            System.out.println("Status książki: " + (entry.getValue().getRead() ? "Przeczytana": "Nieprzeczytana"));
+            System.out.println("Opis: " + entry.getValue().getDescription());
+        }
+    }
 }
