263a92a591ab8aa3fa14506e0242977de372a464
Jarek Szczepanik <ten.ancymon@gmail.com>
2020-05-10 11:46:56 +0200

    Szkielet aplikacji webowej v.02

--- BookShare/web-app/src/main/java/MotoHafty/servlet/StartServlet.java
+++ BookShare/web-app/src/main/java/MotoHafty/servlet/StartServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/start")
+@WebServlet("/")
 public class StartServlet extends HttpServlet {
 
     @Override
